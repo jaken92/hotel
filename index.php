1205170353663f3af9002264f9ab2b393590a617
jaken92 <jaaakeeen@gmail.com>
2022-12-27 22:33:13 +0100

    makeBooking.php putting input data into db

--- index.php
+++ index.php
@@ -14,13 +14,28 @@ require 'calendar.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="calendar.css">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 
 <body>
-    <div class="calendar-container">
-        <div class="calendar">
-            <?= $calendar->display(date('2023-01-01')); ?>
+    <div class="section-one">
+        <div class="calendar-container">
+            <div class="calendar">
+                <?= $calendar->display(date('2023-01-01')); ?>
+            </div>
+        </div>
+        <div>
+            <form method="post" action="makeBooking.php">
+                <label for="startdate">Enter the startdate of your stay</label>
+                <input type="text" name="startdate">
+                <!-- <input type="date" name="startdate" min="2023-01-01" max="2023-01-31"> -->
+                <label for="enddate">Enter your checkout day</label>
+                <input type="text" name="enddate">
+                <!-- <input type="date" name="enddate" min="2023-01-01" max="2023-01-31"> -->
+                <button>Book your stay</button>
+
+            </form>
         </div>
     </div>
 
