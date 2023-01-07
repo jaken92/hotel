e52df0af80edca83c616db77fb26b30090f6df3a
jaken92 <jaaakeeen@gmail.com>
2023-01-07 21:39:13 +0100

    resized images , added some css

--- index.php
+++ index.php
@@ -43,52 +43,90 @@ require 'calendar.php';
     </header>
     <div class="section-one">
         <div class="hero-container">
-            <img class="hero" src="images/pexels-julia-volk-7293098.jpg" alt="">
+            <img class="hero" src="images/banner.png" alt="">
         </div>
     </div>
-    <div class="section-two">
-        <div class="calendar-container">
-            <div class="calendar">
-                <?= $calendarBasic->display(date('2023-01-01')); ?>
+    <div class="sections">
+        <h2>Basic</h2>
+        <h3>Beachview</h3>
+        <div class="basic-content">
+            <div class="calendar-container">
+                <div class="calendar">
+                    <?= $calendarBasic->display(date('2023-01-01'), 'green'); ?>
+                </div>
+            </div>
+            <div class="basic-container">
+                <div class="beachimg-container">
+                    <img class="beachimg" src="/images/beachview.png" alt="">
+                </div>
             </div>
         </div>
+        <p>Enjoy this gem of an island fully by sleeping under the blue sky on the beach. Your stay includes a very cosy blanket the we wash occasionaly. If you select this option we warmly reccomend you to preorder the feature "baseball-bat" to fend off the raccoons that occasionaly terrorize our guests during nighttime.
+        </p>
     </div>
-    <div class="section-two">
-        <div class="calendar-container">
-            <div class="calendar">
-                <?= $calendarReg->display(date('2023-01-01')); ?>
+    <div class="sections">
+        <h2>Basic</h2>
+        <h3>Beachview</h3>
+        <div class="regular-content">
+            <div class="calendar-container">
+                <div class="calendar">
+                    <?= $calendarReg->display(date('2023-01-01'), 'green'); ?>
+                </div>
+            </div>
+            <div class="basic-container">
+                <div class="beachimg-container">
+                    <img class="treehouse" src="/images/treehouse.png" alt="">
+                </div>
             </div>
         </div>
+        <p>Enjoy this gem of an island fully by sleeping under the blue sky on the beach. Your stay includes a very cosy blanket the we wash occasionaly. If you select this option we warmly reccomend you to preorder the feature "baseball-bat" to fend off the raccoons that occasionaly terrorize our guests during nighttime. </p>
     </div>
-    <div class="section-two">
-        <div class="calendar-container">
-            <div class="calendar">
-                <?= $calendarLux->display(date('2023-01-01'), 'green'); ?>
+    <div class="sections">
+        <h2>Basic</h2>
+        <h3>Beachview</h3>
+        <div class="luxury-content">
+            <div class="calendar-container">
+                <div class="calendar">
+                    <?= $calendarLux->display(date('2023-01-01'), 'green'); ?>
+                </div>
+            </div>
+            <div class="basic-container">
+                <div class="beachimg-container">
+                    <img class="beachimg" src="/images/bungalow.png" alt="">
+                </div>
             </div>
         </div>
+        <p>Enjoy this gem of an island fully by sleeping under the blue sky on the beach. Your stay includes a very cosy blanket the we wash occasionaly. If you select this option we warmly reccomend you to preorder the feature "baseball-bat" to fend off the raccoons that occasionaly terrorize our guests during nighttime. </p>
     </div>
     <div class="section-form">
-        <form method="post" action="makeBooking.php">
-            <label for="startdate">Enter the startdate of your stay</label>
-            <!-- <input type="text" name="startdate"> -->
-            <input type="date" name="startdate" min="2023-01-01" max="2023-01-31">
-            <label for="enddate">Enter your checkout day</label>
-            <!-- <input type="text" name="enddate"> -->
-            <input type="date" name="enddate" min="2023-01-01" max="2023-01-31">
-            <label for="room">Choose room type</label>
-            <select name="room" id="">
-                <option value="basic">Basic</option>
-                <option value="regular">Regular</option>
-                <option value="luxury">Luxury</option>
-            </select>
-            <label for="transfercode">Enter your transfercode below</label>
-            <input type="text" name="transfercode" placeholder="Paste transfercode here">
-            <div class="features-container">
+        <h2>Book your stay</h2>
+        <div class="booking-content">
+            <div>
+                <form method="post" action="makeBooking.php">
+                    <label for="startdate">Enter the startdate of your stay</label>
+                    <!-- <input type="text" name="startdate"> -->
+                    <input type="date" name="startdate" min="2023-01-01" max="2023-01-31">
+                    <label for="enddate">Enter your checkout day</label>
+                    <!-- <input type="text" name="enddate"> -->
+                    <input type="date" name="enddate" min="2023-01-01" max="2023-01-31">
+                    <label for="room">Choose room type</label>
+                    <select name="room" id="">
+                        <option value="basic">Basic</option>
+                        <option value="regular">Regular</option>
+                        <option value="luxury">Luxury</option>
+                    </select>
+                    <label for="transfercode">Enter your transfercode below</label>
+                    <input type="text" name="transfercode" placeholder="Paste transfercode here">
+                    <div class="features-container">
 
+                    </div>
+                    <button>Book your stay</button>
+                </form>
             </div>
-            <button>Book your stay</button>
-
-        </form>
+            <div class="bar-container">
+                <img class="bar-img" src="/images/bar.png" alt="">
+            </div>
+        </div>
     </div>
     <ul class="features">
 
